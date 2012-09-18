7813d703e2d9573abc4844b56fa04b62e7badc2c
Ken Peltzer <kpeltzer@thrillist.com>
2012-09-18 11:52:10 -0400

    Initial refactor commit

--- Lib/Cache/Engine/ThrillistMemcacheEngine.php
+++ Lib/Cache/Engine/ThrillistMemcacheEngine.php
@@ -1,14 +1,15 @@
 <?php
 App::uses('MemcacheEngine', 'Cache/Engine');
 /**
- * ViewMemcache storage engine for cache.  This cache keeps '/'s in the key.
+ * Thrillist storage engine for cache.  This cache keeps '/'s in the key.
+ * Based off of ViewMemcache engine (https://github.com/salgo/cakephp-viewmemcache) with added
+ * Thrillist-related logic.
  * Memcache has some limitations in the amount of
  * control you have over expire times far in the future.  See MemcacheEngine::write() for
  * more information.
  *
- * @author rynop.com
  */
-class ViewMemcacheEngine extends MemcacheEngine {
+class ThrillistMemcacheEngine extends MemcacheEngine {
 	/**
 	 * Don't convert / to _
 	 *
@@ -22,4 +23,6 @@ class ViewMemcacheEngine extends MemcacheEngine {
 // 		CakeLog::write('debug', "ViewMemCacheEngine: key (minus cache prefix): {$key}");
 		return $key;
 	}
+
+	public function 
 }
